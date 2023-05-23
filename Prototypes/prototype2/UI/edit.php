31e3bac75448bb561d706ed262a160d82a3560aa
husseinbouik <116066389+husseinbouik@users.noreply.github.com>
2023-05-23 10:35:50 +0100

    file rouge

--- Prototypes/prototype2/UI/edit.php
+++ Prototypes/prototype2/UI/edit.php
@@ -33,61 +33,148 @@ if (isset($_POST['modifier'])) {
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style/file.css">
-    <title>Modifier : <?=$expert->getName()?></title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="./style/file.css">
+  <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+  <link href="assets/lab-sb-admin/dist/css/styles.css" rel="stylesheet" />
+  <link href="assets/lab-sb-admin/dist/css/custom.css" rel="stylesheet" />
+  <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+
+  <title>Gestion des experts</title>
 </head>
 
-<body>
+<body class="sb-nav-fixed">
+  <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+    <!-- Navbar Brand-->
+    <a class="navbar-brand ps-3" href="index.html">Mentorini</a>
+    <!-- Sidebar Toggle-->
+    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+  </nav>
+  <div id="layoutSidenav">
+    <div id="layoutSidenav_nav">
+      <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+        <div class="sb-sidenav-menu">
+          <div class="nav">
+            <div class="sb-sidenav-menu-heading">Core</div>
+            <a class="nav-link" href="index.php">
+              <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+              Dashboard
+            </a>
 
-    <h1>Modification de l'Expert : <?=$expert->getName()?></h1>
-    <form method="post" action="">
-        <input type="text" required="required"
-            id="Id" name="Id_Expert"
-            value="<?php echo $expert->getId()?>">
+            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+              <nav class="sb-sidenav-menu-nested nav">
+                <a class="nav-link" href="layout-static.html">Static Navigation</a>
+                <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+              </nav>
+            </div>
 
-        <div>
-            <label for="name">Name</label>
-            <input type="text" required="required"
-            id="name" name="name"  placeholder="Name"
-            value="<?php echo $expert->getName()?> ">
-        </div>
-      
-        <div>
-            <label for="expertise">Expertise</label>
-            <input type="text" required="required"
-            id="expertise" name="expertise" placeholder="Expertise"
-            value="<?php echo $expert->getExpertise()?>">
-        </div>
+            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+              <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
+                  Authentication
+                  <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
 
-        <div>
-            <label for="email">Email</label>
-            <input type="email" required="required"
-            id="email" name="email" placeholder="Email"
-            value="<?php echo $expert->getEmail()?>">
-        </div>
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
+                  Error
+                  <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
 
-        <div>
-            <label for="phone">Phone</label>
-            <input type="text" required="required"
-            id="phone" name="phone" placeholder="Phone"
-            value="<?php echo $expert->getPhone()?>">
-        </div>
+              </nav>
+            </div>
 
-        <div>
-            <label for="location">Location</label>
-            <input type="text" required="required"
-            id="location" name="location" placeholder="Location"
-            value="<?php echo $expert->getLocation()?>">
+          </div>
         </div>
+        <div class="sb-sidenav-footer">
+          <div class="small">Logged in as:</div>
+          Hussein Bouik
+        </div>
+      </nav>
+    </div>
+    <div id="layoutSidenav_content">
+      <main>
+        <div class="container-fluid px-4">
+          <h1 class="mt-4">Credentials</h1>
+          <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">Credentials</li>
+          </ol>
+          <div class="card mb-4">
+            <div class="card-header">
+              <i class="fas fa-table me-1"></i>
+              Gestion des Credentials
+            </div>
+            <div class="card-body">
+              <div class="container">
+              <h1>Modification de l'Expert : <?=$expert->getName()?></h1>
+              <form method="post" action="" class="needs-validation">
+  <input type="hidden" required="required" id="Id" name="Id_Expert" value="<?php echo $expert->getId()?>">
 
-        <div>
-            <input name="modifier" type="submit" value="Modifier">
-            <a href="../index.php">Annuler</a>
+  <div class="form-group">
+    <label for="name">Name</label>
+    <input type="text" required="required" id="name" name="name" placeholder="Name" value="<?php echo $expert->getName()?>" class="form-control">
+  </div>
+
+  <div class="form-group">
+    <label for="expertise">Expertise</label>
+    <input type="text" required="required" id="expertise" name="expertise" placeholder="Expertise" value="<?php echo $expert->getExpertise()?>" class="form-control">
+  </div>
+
+  <div class="form-group">
+    <label for="email">Email</label>
+    <input type="email" required="required" id="email" name="email" placeholder="Email" value="<?php echo $expert->getEmail()?>" class="form-control">
+  </div>
+
+  <div class="form-group">
+    <label for="phone">Phone</label>
+    <input type="text" required="required" id="phone" name="phone" placeholder="Phone" value="<?php echo $expert->getPhone()?>" class="form-control">
+  </div>
+
+  <div class="form-group">
+    <label for="location">Location</label>
+    <input type="text" required="required" id="location" name="location" placeholder="Location" value="<?php echo $expert->getLocation()?>" class="form-control">
+  </div>
+
+  <div class="form-group">
+    <input name="modifier" type="submit" value="Modifier" class="btn btn-primary">
+    <a href="../index.php">Annuler</a>
+  </div>
+</form>
+      </div>
+
+
+            </div>
+          </div>
         </div>
-    </form>
+      </main>
+      <footer class="py-4 bg-light mt-auto">
+        <div class="container-fluid px-4">
+          <div class="d-flex align-items-center justify-content-between small">
+            <div class="text-muted">Copyright &copy; Your Website 2023</div>
+            <div>
+              <a href="#">Privacy Policy</a>
+              &middot;
+              <a href="#">Terms &amp; Conditions</a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+  <script src="assets/lab-sb-admin/dist/js/scripts.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+  <script src="assets/lab-sb-admin/dist/assets/demo/chart-area-demo.js"></script>
+  <script src="assets/lab-sb-admin/dist/assets/demo/chart-bar-demo.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+  <script src="assets/lab-sb-admin/dist/js/datatables-simple-demo.js"></script>
 </body>
 
 </html>
+
+
+
+  
--- Prototypes/prototype2/UI/editcredential.php
+++ Prototypes/prototype2/UI/editcredential.php
@@ -27,44 +27,140 @@ if (isset($_POST['modifier'])) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style/file.css">
-    <title>Modify: <?= $credential->getExpertId() ?></title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="./style/file.css">
+  <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+  <link href="assets/lab-sb-admin/dist/css/styles.css" rel="stylesheet" />
+  <link href="assets/lab-sb-admin/dist/css/custom.css" rel="stylesheet" />
+  <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+
+  <title>Gestion des experts</title>
 </head>
 
-<body>
-    <h1>Modification of Credential: <?= $credential->getDegree() ?></h1>
-    <form method="post" action="">
-        <input type="hidden" required="required" name="Credential_ID" value="<?= $credential->getCredentialId() ?>">
+<body class="sb-nav-fixed">
+  <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+    <!-- Navbar Brand-->
+    <a class="navbar-brand ps-3" href="index.html">Mentorini</a>
+    <!-- Sidebar Toggle-->
+    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+  </nav>
+  <div id="layoutSidenav">
+    <div id="layoutSidenav_nav">
+      <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+        <div class="sb-sidenav-menu">
+          <div class="nav">
+            <div class="sb-sidenav-menu-heading">Core</div>
+            <a class="nav-link" href="index.php">
+              <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+              Dashboard
+            </a>
 
-        <div>
-            <label for="degree">Degree</label>
-            <input type="text" required="required" id="degree" name="degree" placeholder="Degree"
-                value="<?= $credential->getDegree() ?>">
-        </div>
+            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+              <nav class="sb-sidenav-menu-nested nav">
+                <a class="nav-link" href="layout-static.html">Static Navigation</a>
+                <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+              </nav>
+            </div>
 
-        <div>
-            <label for="institution">Institution</label>
-            <input type="text" required="required" id="institution" name="institution" placeholder="Institution"
-                value="<?= $credential->getInstitution() ?>">
-        </div>
+            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+              <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
+                  Authentication
+                  <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
 
-        <div>
-            <label for="year">Year</label>
-            <input type="text" required="required" id="year" name="year" placeholder="Year"
-                value="<?= $credential->getYear() ?>">
-        </div>
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
+                  Error
+                  <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
 
-        <div>
-            <input name="modifier" type="submit" value="Modify">
-            <a href="credentials.php?Id_Expert=<?php echo $credential->getExpertId() ?>">Cancel</a>
+              </nav>
+            </div>
+
+          </div>
         </div>
-    </form>
+        <div class="sb-sidenav-footer">
+          <div class="small">Logged in as:</div>
+          Hussein Bouik
+        </div>
+      </nav>
+    </div>
+    <div id="layoutSidenav_content">
+      <main>
+        <div class="container-fluid px-4">
+          <h1 class="mt-4">Credentials</h1>
+          <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">Credentials</li>
+          </ol>
+          <div class="card mb-4">
+            <div class="card-header">
+              <i class="fas fa-table me-1"></i>
+              Gestion des Credentials
+            </div>
+            <div class="card-body">
+              <div class="container">
+              <h1>Modification of Credential: <?= $credential->getDegree() ?></h1>
+              <form method="post" action="" class="needs-validation">
+  <input type="hidden" required="required" name="Credential_ID" value="<?= $credential->getCredentialId() ?>">
+
+  <div class="form-group">
+    <label for="degree">Degree</label>
+    <input type="text" required="required" id="degree" name="degree" placeholder="Degree"
+      value="<?= $credential->getDegree() ?>" class="form-control">
+  </div>
+
+  <div class="form-group">
+    <label for="institution">Institution</label>
+    <input type="text" required="required" id="institution" name="institution" placeholder="Institution"
+      value="<?= $credential->getInstitution() ?>" class="form-control">
+  </div>
+
+  <div class="form-group">
+    <label for="year">Year</label>
+    <input type="text" required="required" id="year" name="year" placeholder="Year"
+      value="<?= $credential->getYear() ?>" class="form-control">
+  </div>
+
+  <div class="form-group">
+    <input name="modifier" type="submit" value="Modify" class="btn btn-primary">
+    <a href="credentials.php?Id_Expert=<?php echo $credential->getExpertId() ?>">Cancel</a>
+  </div>
+</form>
+  </div>
+
+
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer class="py-4 bg-light mt-auto">
+        <div class="container-fluid px-4">
+          <div class="d-flex align-items-center justify-content-between small">
+            <div class="text-muted">Copyright &copy; Your Website 2023</div>
+            <div>
+              <a href="#">Privacy Policy</a>
+              &middot;
+              <a href="#">Terms &amp; Conditions</a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+  <script src="assets/lab-sb-admin/dist/js/scripts.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+  <script src="assets/lab-sb-admin/dist/assets/demo/chart-area-demo.js"></script>
+  <script src="assets/lab-sb-admin/dist/assets/demo/chart-bar-demo.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+  <script src="assets/lab-sb-admin/dist/js/datatables-simple-demo.js"></script>
 </body>
 
 </html>
